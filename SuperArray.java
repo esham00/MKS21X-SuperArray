64cba1975d438aab7a29bdd3740e274fe6fe9fde
Elizabeth Sham <esham00@stuy.edu>
2018-10-24 02:48:11 -0400

    add not working

--- SuperArray.java
+++ SuperArray.java
@@ -5,7 +5,12 @@ public class SuperArray {
 		if (s > 10) {
 			s = 10;
 		}
-		data = new String[s];
+		size = s;
+		data = new String[size];
+	}
+	public SuperArray() {
+		size = 10;
+		data = new String[size];
 	}
 	public void clear() {
 		size = 0;
@@ -19,37 +24,74 @@ public class SuperArray {
 		else {return false;}
 	}
 	public boolean add(String element) {
+		if (size == 0) {
+			resize();
+		}
 		for(int i = 0; i < size; i++) {
 			if (data[i] == null) {
 				data[i] = element;
 				return true;
 			}
+			if (data[size-1] != null) {
+				resize();
+				i = -1;
+			}
 		}
+		return false;
 	}
 	public String toString() {
-		System.out.print("\"[");
+		String brandnew = "[";
 		for(int i = 0; i < size; i++) {
-			if (i = 0) {
-				System.out.print(data[i])
+			if (data[i] == null) {
+				i++;
+			}
+			else if (i == 0) {
+				brandnew += data[i];
 			}
 			else {
-				System.out.print("," + data[i]);
+				brandnew += ", " + data[i];
 			}
 		}
-		System.out.print("]\""
+		brandnew += "]";
+		return brandnew;
+	}
+	public String toStringDebug() {
+		String brandnew = "[";
+		for(int i = 0; i < size; i++) {
+			if (i == 0) {
+				brandnew += data[i];
+			}
+			else {
+				brandnew += ", " + data[i];
+			}
+		}
+		brandnew += "]";
+		return brandnew;
 	}
 	public String get(int index) {
-		if (index < 0 || index >= size()) {
+		if (index < 0 || index >= size) {
 			return null;
 		}
 		return data[index];
 	}
 	public String set(int index, String element) {
-		if (index < 0 || index >= size()) {
+		if (index < 0 || index >= size) {
 			return null;
 		}
-		oldElement = data[index];
+		String oldElement = data[index];
 		data[index] = element;
 		return oldElement;
 	}
-
+	private void resize() {
+		if (size == 0) {
+			size = 1;
+			data = new String[size];
+		}
+		size++;
+		String[] original = new String[size-1];
+		data = new String[size];
+		for (int i = 0; i < size-1; i++) {
+			data[i] = original[i];
+		}
+	}
+}
