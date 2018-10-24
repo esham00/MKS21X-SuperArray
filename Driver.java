3bfa91e376f072b0d6648d12d8f9ae822ce42cb7
Elizabeth Sham <esham00@stuy.edu>
2018-10-24 19:11:34 -0400

    works w my driver

--- Driver.java
+++ Driver.java
@@ -1,2 +1,33 @@
 public class Driver {
-
+  public static void main(String[] args) {
+    SuperArray sad, happy;
+    sad = new SuperArray(5);
+    happy = new SuperArray();
+    System.out.println(sad.size() + ": 0");
+    System.out.println(happy.size() + ": 0");
+    sad.add("homework");
+    sad.add("more homework");
+    sad.add("even more homework");
+    happy.add("less homework");
+    System.out.println(happy.add("no homework") + ": true");
+    System.out.println(sad.size() + ": 3");
+    System.out.println(happy.size() + ": 2");
+    sad.clear();
+    System.out.println(sad.isEmpty() + ": true");
+    System.out.println(happy.isEmpty() + ": false");
+    sad.add("assigned hw");
+    happy.add("found money");
+    System.out.println(happy + ": [less homework, no homework, found money]");
+    System.out.println(sad + ": [assigned hw]");
+    System.out.println(happy.toStringDebug() + ": same as happy but + 7 null values after found money");
+    System.out.println(sad.toStringDebug() + ": same as sad");
+    System.out.println(happy.get(0) + ": less homework");
+    System.out.println(sad.get(3) + ": null");
+    System.out.println(happy.set(1, "graduated") + ": no homework");
+    System.out.println(sad.add("stuck in stuy") + ": true");
+    for (int i = 0; i < 8; i++) {
+      sad.add("sad");
+    }
+    System.out.println(sad + ": should have 10 values");
+  }
+}
