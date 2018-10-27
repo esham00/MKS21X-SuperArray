8265224f1194e3c9f4f60155da88e91cf5559108
Elizabeth Sham <esham00@stuy.edu>
2018-10-27 12:32:40 -0400

    forgot to pull

--- Driver.java
+++ Driver.java
@@ -46,5 +46,13 @@ public class Driver {
     System.out.println(sad);
     System.out.println(happy);
     sad.add(200, "life");
+    System.out.print(": error");
+    sad.remove("life");
+    sad.remove(1);
+    happy.remove(3);
+    happy.remove(2);
+    System.out.println(sad);
+    System.out.println(happy);
+    sad.remove(200);
   }
 }
